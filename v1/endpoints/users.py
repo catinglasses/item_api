d43d88345e516dad59297d419167a6f52e94dab9
Saveliy Efimov <savva.cat@mail.ru>
2024-11-26 12:22:42 +0300

    add python-status lib dependency, remake users endpoints (in progress)

--- v1/endpoints/users.py
+++ v1/endpoints/users.py
@@ -1,29 +1,53 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import timedelta
+import status
 
-from src.schemas.users import UserCreate, UserLogin, UserOut
-from src.services.user_service import UserService
-from src.services.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 from src.models.users import User
-from src.models.database import get_db
 from src.repositories.user_repository import UserRepository
+from src.services.user_service import UserService
+from src.services.auth_service import TokenService, PasswordManager, UserAuthenticationManager
+from src.services.auth_service import ACCESS_TOKEN_EXPIRE_MINUTES
+from src.schemas.users import UserCreate, UserLogin, UserOut
+from src.models.database import get_db
 
 router = APIRouter()
 
-async def get_user_repository(db: AsyncSession = Depends(get_db)) -> UserRepository:
+async def get_user_repository(
+    db: AsyncSession = Depends(get_db)
+) -> UserRepository:
+    """Create UserRepository instance, dependency inject async db session"""
     return UserRepository(db)
+
+async def get_password_manager(
+    user_repository: UserRepository = Depends(get_user_repository)
+) -> PasswordManager:
+    """Create PasswordManager instance, dependency inject UserRepository"""
+    return PasswordManager(user_repository)
+
+async def get_token_service(
+    user_repository: UserRepository = Depends(get_user_repository)
+) -> TokenService:
+    """Create TokenService instance, dependency inject UserRepository"""
+    return TokenService(user_repository)
+
+async def get_authentication_manager(
+    token_service: TokenService = Depends(get_token_service),
+    password_manager: PasswordManager = Depends(get_password_manager)
+) -> UserAuthenticationManager:
+    """Create UserAuthenticationManager, DI TokenService & PasswordManager"""
+    return UserAuthenticationManager(token_service, password_manager)
 
 @router.post("/token")
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
-    user_repository: UserRepository = Depends(get_user_repository)
+    auth_manager: UserAuthenticationManager = Depends(get_authentication_manager)
 ):
     """Endpoint to log in users"""
-    user_service = UserService(user_repository)
-
     user_login = UserLogin(username=form_data.username, password=form_data.password)
-    user = user_service.authenticate_user(user_login=user_login)
+
+    user = await auth_manager.authenticate_user(user_login=user_login)
 
     if not user:
         raise HTTPException(
@@ -33,14 +57,8 @@ async def login(
         )
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+    access_token = auth_manager.token_service.create_access_token(
+        data={"sub": user.username}, expires_detla=access_token_expires
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
-
-@router.post("/user/create/", response_model=User)
-async def register(
-
-):
